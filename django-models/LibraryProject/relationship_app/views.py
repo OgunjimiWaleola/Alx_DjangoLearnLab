915a51ceade06f9211c67a3259b054dec75f08de
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-02-09 16:00:01 +0100

    FBV and CBV, urls, templates fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,18 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render
 from .models import Book, Library
+from django.views.generic import DetailView
 
-
-# -------------------------
 # Function-Based View
-# -------------------------
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-
-    return render(request, "relationship_app/list_books.html", {
-        "books": books
-    })
+    books = Book.objects.all() 
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# -------------------------
 # Class-Based View
-# -------------------------
+from django.views.generic import DetailView
+from .models import Library
+
 class LibraryDetailView(DetailView):
-    model = Library
+    model = Library  
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
