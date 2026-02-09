34ee0f9e10be4a2eb26dc5863c520b9d42c256df
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-02-09 16:24:03 +0100

    Fix imports, FBV, CBV, urls, templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Book, Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView   
+
 
 # Function-Based View
 def list_books(request):
-    books = Book.objects.all() 
+    books = Book.objects.all()  
     return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 
 # Class-Based View
@@ -13,6 +15,6 @@ from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
-    model = Library  
+    model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
