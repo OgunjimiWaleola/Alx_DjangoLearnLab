9f6989c376bcb934b9269d7cfbe6aab7807f25f8
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-02-23 13:50:33 +0100

    Connect api.urls in main project urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')),  
 ]
