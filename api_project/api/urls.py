b892d3cc7e1e7a1cd702c5457f737daf740b5b71
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-02-23 14:36:43 +0100

    Add router and register BookViewSet for full CRUD

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# Create router and register ViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Existing ListAPIView
     path('books/', BookList.as_view(), name='book-list'),
-]
 
+    # Include all CRUD routes from router
+    path('', include(router.urls)),
+]
