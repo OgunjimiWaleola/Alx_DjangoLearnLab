2e91b4edf6f80a4a8f70a18ce19f8ebdad425389
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-03-04 12:57:47 +0100

    Fix URL patterns to match  CRUD paths (post/new, post/<pk>/update, post/<pk>/delete)

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,14 +16,11 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
 
-
-urlpatterns += [
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
 
