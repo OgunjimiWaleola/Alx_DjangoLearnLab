3b8d8510a608b5cf7ca0b7ff8b9645cb359bff58
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-03-09 15:00:43 +0100

    Add database PORT configuration for deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -88,11 +88,16 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),  
+    }
 }
 
 
